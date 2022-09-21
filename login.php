91222b94908a13385f7a27d28ec1fb70c4685183
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-21 19:49:02 +0300

    Adding a login API(#2)

---
+++ login.php
@@ -0,0 +1,20 @@
+<?phpheader('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connection.php");
+
+$email = $_POST["client_email"];
+$password = hash("sha256", $_POST["client_password"]);
+
+$query = $mysqli->prepare("SELECT id FROM clients WHERE client_email = ? and client_password = ? LIMIT 1");
+$query->bind_param("ss", $email, $password);
+$query->execute();
+
+$return = $query -> get_result();
+$result = $return -> fetch_assoc();
+
+
+echo json_encode($result['id']);
+
+?>
