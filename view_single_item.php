1408c73d8ad4a33404b97397a5844ca508c7c6a3
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-22 06:58:35 +0300

    Adding a single view API(#7)

--- view_single_item.php
+++ view_single_item.php
@@ -8,15 +8,9 @@ include("connections.php");
 $client = $_POST["client_id"];
 $product = $_POST["product_id"];
 
-$query = $mysqli->prepare("SELECT name, image, price, nb_of_views, rating, category
+$query = $mysqli->prepare("SELECT name, image, price, nb_of_views, rating, category, description
  FROM products,users_view_products WHERE client_id = ?, product_id = ?");
 $query->bind_param("ss", $client , $product);
 $query->execute();
 
-$return = $query -> get_result();
-$result = $return -> fetch_assoc();
-
-$response = [];
-$response["success"] = true;
-echo json_encode($result);
 ?>
