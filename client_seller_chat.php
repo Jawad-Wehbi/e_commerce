96f9597584dd37cdc9336afdcb922fa5ddd653e2
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-22 14:55:58 +0300

    Adding a chat API

---
+++ client_seller_chat.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connections.php");
+
+$client = $_POST["client_id"];
+$seller = $_POST["seller_id"];
+$text = $_POST["text"];
+
+$query = $mysqli->prepare("INSERT INTO clients_sellers_chat(text)");
+VALUE (?) WHERE client_id = ?, seller_id = ?");
+$query->bind_param("sss",$text , $client , $seller);
+$query->execute();
+
+$return = $query -> get_result();
+$result = $return -> fetch_assoc();
+
+$response = [];
+$response["success"] = true;
+echo json_encode($result);
+?>
