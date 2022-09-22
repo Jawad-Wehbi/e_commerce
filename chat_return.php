e217df5ac16a024f779622490482644bb7d8422a
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-22 15:58:17 +0300

    Adding a chat return API

---
+++ chat_return.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connections.php");
+
+$client = $_POST["client_id"];
+$seller = $_POST["seller_id"];
+$text = $_POST["text"];
+
+$query = $mysqli->prepare("SELECT text ,FROM clients_sellers_chat WHERE client_id = ?, seller_id = ?");
+$query->bind_param("sss",$text , $client , $seller);
+$query->execute();
+
+
+$return = $query -> get_result();
+$result = $return -> fetch_assoc();
+
+
+echo json_encode($result);
+?>
