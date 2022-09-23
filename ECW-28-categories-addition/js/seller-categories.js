059280a4ac67a0f9ddc5558d4f5237c9aa013011
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-23 09:08:40 +0300

    ECW-28 <add functionality to add new category btn>

--- ECW-28-categories-addition/js/seller-categories.js
+++ ECW-28-categories-addition/js/seller-categories.js
@@ -1,3 +1,24 @@
 window.onload = () => {
-    console.log("HI");
+    //
+    // Variables
+    //
+    const addNewCategoryBtn = document.getElementById("add-new-category-btn");
+    const categoriesPage = document.getElementById("seller-categories-page");
+    const newCategoryPage = document.getElementById("seller-new-category-page");
+
+    //
+    // Functions
+    //
+    const openNewCategoryPage = () => {
+        categoriesPage.classList.add("seller-popup-hidden");
+        newCategoryPage.classList.remove("seller-popup-hidden");
+    };
+
+    //
+    //
+    //
+    addNewCategoryBtn.addEventListener("click", e => {
+        e.preventDefault();
+        openNewCategoryPage();
+    });
 };
