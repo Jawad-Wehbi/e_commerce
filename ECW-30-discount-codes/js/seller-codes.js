8e029347fc8cda88d57ef2070b9468ee44b9957f
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-23 08:00:26 +0300

    ECW-30 <add functionality to add new code btn>

--- ECW-30-discount-codes/js/seller-codes.js
+++ ECW-30-discount-codes/js/seller-codes.js
@@ -1,3 +1,24 @@
 window.onload = () => {
-    console.log("hello!");
+    //
+    // Variables
+    //
+    const newCodeBtn = document.getElementById("add-new-code-btn");
+    const codesPage = document.getElementById("seller-codes");
+    const newCodePage = document.getElementById("seller-new-code");
+
+    //
+    // Functions
+    //
+    const openNewCodePage = () => {
+        codesPage.classList.add("seller-popup-hidden");
+        newCodePage.classList.remove("seller-popup-hidden");
+    };
+
+    //
+    //
+    //
+    newCodeBtn.addEventListener("click", e => {
+        e.preventDefault();
+        openNewCodePage();
+    });
 };
