c594f436167c13150d9ec2584050ba4bdc9c78ab
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-23 09:21:53 +0300

    ECW-28 <add functionality to close new categ btn>

--- ECW-28-categories-addition/js/seller-categories.js
+++ ECW-28-categories-addition/js/seller-categories.js
@@ -5,6 +5,15 @@ window.onload = () => {
     const addNewCategoryBtn = document.getElementById("add-new-category-btn");
     const categoriesPage = document.getElementById("seller-categories-page");
     const newCategoryPage = document.getElementById("seller-new-category-page");
+    const closeNewCategoryBtn = document.getElementById(
+        "close-new-category-btn"
+    );
+    const newCategoryNameInput = document.getElementById(
+        "new-category-name-input"
+    );
+    const newCategoryDescriptionInput = document.getElementById(
+        "new-category-descriptipon-input"
+    );
 
     //
     // Functions
@@ -13,6 +22,12 @@ window.onload = () => {
         categoriesPage.classList.add("seller-popup-hidden");
         newCategoryPage.classList.remove("seller-popup-hidden");
     };
+    const closeNewCategoryPage = () => {
+        newCategoryNameInput.value = "";
+        newCategoryDescriptionInput.value = "";
+        categoriesPage.classList.remove("seller-popup-hidden");
+        newCategoryPage.classList.add("seller-popup-hidden");
+    };
 
     //
     //
@@ -20,5 +35,9 @@ window.onload = () => {
     addNewCategoryBtn.addEventListener("click", e => {
         e.preventDefault();
         openNewCategoryPage();
+    });
+    closeNewCategoryBtn.addEventListener("click", e => {
+        e.preventDefault();
+        closeNewCategoryPage();
     });
 };
