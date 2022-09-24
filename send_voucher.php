07bb02f6af371f445c95c0c8c20d6d508aa5bc10
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-25 01:24:04 +0300

    Updating API  to be able to receive axios data

--- send_voucher.php
+++ send_voucher.php
@@ -4,10 +4,13 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connections.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
-$client1 = $_POST["client_user_id"];
-$client2 = $_POST["client_user_id1"];
-$voucher = $_POST["vouchers_id"];
+
+$client1 = $data["client_user_id"];
+$client2 = $data["client_user_id1"];
+$voucher = $data["vouchers_id"];
 
 $query = $mysqli->prepare("INSERT INTO clients_send_voucher_to_clients (client_user_id , client_user_id1, vouchers_id) VALUES(?,?,?)");
 $query->bind_param("sss", $client1, $client2, $voucher);
