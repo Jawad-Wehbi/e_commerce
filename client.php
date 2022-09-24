29fc120abe41742ec5e464dc4e2980fcdd267a2b
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-25 01:14:43 +0300

    Updating API  to be able to receive axios data

--- client.php
+++ client.php
@@ -4,8 +4,11 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connections.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
-$id = $_POST["user_id"];
+
+$id = $data["user_id"];
 
 $query = $mysqli->prepare("SELECT * FROM clients WHERE user_id = ?");
 $query->bind_param("i", $id);
