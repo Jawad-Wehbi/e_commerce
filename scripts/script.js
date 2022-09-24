7f8b6ef7923e65039671e8d082b8212e878419de
JaafarHawli <jaafarhawle@gmail.com>
2022-09-24 12:36:55 +0300

    ECW-43 added functionality to navbar elements

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,22 @@ const product = document.querySelectorAll('.product');
 const productModal = document.getElementById('productModal');
 const quit = document.getElementById('quit');
 
+// Header buttons
+const navItems = document.querySelectorAll('.nav-items-element');
+const sections = document.querySelectorAll('.section');
+const footer = document.querySelector('.footer');
+/* const home = document.getElementById('home');
+const categories = document.getElementById('categories');
+const favourites = document.getElementById('favourites');
+const wishlist = document.getElementById('wishlist');
+const cart = document.getElementById('cart');
+const chatPage = document.getElementById('chatPage');
+const account = document.getElementById('accout');
+const wishlistIcon = document.getElementById('wishlistIcon');
+const cartIcon = document.getElementById('cartIcon'); */
+
+const chatContainer = document.querySelector('.chat-container');
+
 // Show product modal on click
 product[0].addEventListener('click', () => {
 	productModal.showModal();
@@ -64,3 +80,64 @@ function displaySlides(num) {
 	}
 	slides[currentIndex - 1].style.display = 'block';
 }
+
+// Client Page Buttons Functionality
+
+// Home
+navItems[0].addEventListener('click', () => {
+	navItems.forEach((element) => element.classList.remove('current'));
+	navItems[0].classList.add('current');
+	sections.forEach((element) => element.classList.add('display'));
+	sections[0].classList.remove('display');
+	footer.classList.remove('display');
+});
+
+// Categories
+navItems[1].addEventListener('click', () => {
+	navItems.forEach((element) => element.classList.remove('current'));
+	navItems[1].classList.add('current');
+	sections.forEach((element) => element.classList.add('display'));
+	sections[1].classList.remove('display');
+	footer.classList.remove('display');
+});
+
+navItems[2].addEventListener('click', () => {
+	navItems.forEach((element) => element.classList.remove('current'));
+	navItems[2].classList.add('current');
+	sections.forEach((element) => element.classList.add('display'));
+	sections[2].classList.remove('display');
+	footer.classList.remove('display');
+});
+
+navItems[3].addEventListener('click', () => {
+	navItems.forEach((element) => element.classList.remove('current'));
+	navItems[3].classList.add('current');
+	sections.forEach((element) => element.classList.add('display'));
+	sections[3].classList.remove('display');
+	footer.classList.remove('display');
+});
+
+navItems[4].addEventListener('click', () => {
+	navItems.forEach((element) => element.classList.remove('current'));
+	navItems[4].classList.add('current');
+	sections.forEach((element) => element.classList.add('display'));
+	sections[4].classList.remove('display');
+	footer.classList.remove('display');
+});
+
+navItems[5].addEventListener('click', () => {
+	navItems.forEach((element) => element.classList.remove('current'));
+	navItems[5].classList.add('current');
+	sections.forEach((element) => element.classList.add('display'));
+	sections[5].classList.remove('display');
+	chatContainer.scrollTop = chatContainer.scrollHeight;
+	footer.classList.add('display');
+});
+
+navItems[6].addEventListener('click', () => {
+	navItems.forEach((element) => element.classList.remove('current'));
+	navItems[6].classList.add('current');
+	sections.forEach((element) => element.classList.add('display'));
+	sections[6].classList.remove('display');
+	footer.classList.remove('display');
+});
