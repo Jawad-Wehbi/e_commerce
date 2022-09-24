9c00a735a965712b19f7841ac8478fcb1c0f1b57
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-24 14:43:54 +0300

    Fixing the login API

--- login.php
+++ login.php
@@ -5,17 +5,23 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorizatio
 
 include("connections.php");
 
-$email = $_POST["client_email"];
-$password = hash("sha256", $_POST["client_password"]);
+$email = $_POST["email"];
+// $password = hash("sha256", $_POST["password"]);
+$password = $_POST["password"];
 
-$query = $mysqli->prepare("SELECT id FROM clients WHERE client_email = ? and client_password = ? LIMIT 1");
+$query = $mysqli->prepare("SELECT * FROM users WHERE email = ? and password = ? LIMIT 1");
 $query->bind_param("ss", $email, $password);
 $query->execute();
 
-$return = $query -> get_result();
-$result = $return -> fetch_assoc();
+$array = $query->get_result();
 
+$response = [];
 
-echo json_encode($result['id']);
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
 
 ?>
