ac9898f292a024de33d1042b74bbeff09535da88
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-25 00:48:54 +0300

    Updating API  to be able to receive axios data

--- seller_revenue_week.php
+++ seller_revenue_week.php
@@ -5,9 +5,11 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connection.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
 // get product name from the user
-$seller_user_id = $_POST["seller_user_id"];
+$seller_user_id = $data["seller_user_id"];
 $checkout_date = date('y:m:d',strtotime('-7 days'));
 
 //Sum product from products prices after 1 week
