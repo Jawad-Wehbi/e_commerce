50178f3229f400165de30c33d9c4be5cf3fd62cd
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-24 17:39:50 +0300

    Fixing some bugs

--- add_favourite.php
+++ add_favourite.php
@@ -5,11 +5,16 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorizatio
 
 include("connections.php");
 
-$client = $_POST["client_user_id"];
-$product = $_POST["product_id"];
+$client_user_id = $_POST["client_user_id"];
+$product_id = $_POST["product_id"];
 
-$query = $mysqli->prepare("INSERT INTO client_favorite_products(user_client_id, product_id ) 
+$query = $mysqli->prepare("INSERT INTO client_favorite_products (client_user_id, product_id ) 
 VALUES (?, ?)");
-$query->bind_param("ss", $client, $product);
+$query->bind_param("ii", $client_user_id, $product_id);
 $query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response)
 ?>
