e1ab787a56a583642bd51a79f2e79343846ffcbd
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-25 01:28:13 +0300

    Updating API  to be able to receive axios data

--- client_seller_chat.php
+++ client_seller_chat.php
@@ -4,10 +4,13 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connections.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
-$client_user_id = $_POST["client_user_id"];
-$seller_user_id = $_POST["seller_user_id"];
-$text = $_POST["text"];
+
+$client_user_id = $data["client_user_id"];
+$seller_user_id = $data["seller_user_id"];
+$text = $data["text"];
 
 $query = $mysqli->prepare("INSERT INTO clients_sellers_chat(client_user_id,seller_user_id,text) VALUE (?,?,?) ");
 $query->bind_param("iis", $client_user_id , $seller_user_id,$text);
