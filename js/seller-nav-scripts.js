f330c76cee649be4557f39c042f2e322e58edb08
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-24 18:27:36 +0300

    ECW-24 <add functionality to the left panel tabs>

---
+++ js/seller-nav-scripts.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+    //
+    // Variables
+    //
+    const revenueTab = document.getElementById("revenue-tab");
+    const categoriesTab = document.getElementById("categories-tab");
+    const discountCodesTab = document.getElementById("discount-codes-tab");
+    const messagesTab = document.getElementById("messages-tab");
+    const top5Tab = document.getElementById("top5-tab");
+    const addAdsTab = document.getElementById("add-ads-tab");
+
+    //
+    // Listeners
+    //
+    revenueTab.addEventListener("click", e => {
+        e.preventDefault();
+        location.replace("./revenue.html");
+    });
+    categoriesTab.addEventListener("click", e => {
+        e.preventDefault();
+        location.replace("./categories.html");
+    });
+    discountCodesTab.addEventListener("click", e => {
+        e.preventDefault();
+        location.replace("./discount-codes.html");
+    });
+    messagesTab.addEventListener("click", e => {
+        e.preventDefault();
+        location.replace("./messages.html");
+    });
+    top5Tab.addEventListener("click", e => {
+        e.preventDefault();
+        location.replace("./top-5-products-viewed.html");
+    });
+    addAdsTab.addEventListener("click", e => {
+        e.preventDefault();
+        location.replace("./add-ads.html");
+    });
+};
