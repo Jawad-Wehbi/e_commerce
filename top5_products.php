82b4032f97799a9dca7c44e1b41571a2e325ed91
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-25 00:49:56 +0300

    correcting comments

--- top5_products.php
+++ top5_products.php
@@ -5,12 +5,13 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connection.php");
+
 $request_body = file_get_contents('php://input');
 $data = json_decode($request_body, true);
+
 $seller_user_id = $data["seller_user_id"];
 
 
-// $seller_user_id = $_POST["seller_user_id"];
 //query and get the maximum number of views
 $query = $mysqli->prepare("SELECT nb_of_views ,name,product_id FROM  products WHERE seller_user_id=? ORDER BY nb_of_views DESC LIMIT 5  " );
 $query->bind_param("s",$seller_user_id);
