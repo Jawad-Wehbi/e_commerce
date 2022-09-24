65fa56ff4b146732689851e4bd3b0689676a030b
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-25 01:21:45 +0300

    Updating API  to be able to receive axios data

--- add_to_wishlist.php
+++ add_to_wishlist.php
@@ -4,10 +4,13 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connections.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
-$client = $_POST["client_user_id"];
-$product = $_POST["products_id"];
-$price = $_POST["price"];
+
+$client = $data["client_user_id"];
+$product = $data["products_id"];
+$price = $data["price"];
 
 $query = $mysqli->prepare("INSERT INTO client_add_products_to_wishlist (client_user_id , products_id , price) VALUES(?,?,?)");
 $query->bind_param("sss", $client, $product, $price);
