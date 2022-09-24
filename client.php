46bc60a1f0c840fbc437dfb6bc76ed01dacad9ed
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-24 23:00:59 +0300

    Adding a client API

---
+++ client.php
@@ -0,0 +1,25 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connections.php");
+
+$id = $_POST["user_id"];
+
+$query = $mysqli->prepare("SELECT * FROM clients WHERE user_id = ?");
+$query->bind_param("i", $id);
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
+
+?>
