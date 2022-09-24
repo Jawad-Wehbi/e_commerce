d57c6c956901896234ad92171ba8ab131cec6a85
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-24 14:33:57 +0300

    Added seller revenue in a week

--- seller_revenue_week.php
+++ seller_revenue_week.php
@@ -8,11 +8,11 @@ include("connection.php");
 
 // get product name from the user
 $seller_user_id = $_POST["seller_user_id"];
-
+$checkout_date = date('y:m:d',strtotime('-7 days'));
 
 //Sum product from the view products table
-$query = $mysqli->prepare("SELECT  SUM(checkout_history.price) as revenue , products.seller_user_id  FROM checkout_history,products  WHERE (checkout_history.product_id= products.id and products.seller_user_id=?) ");
-$query->bind_param("s", $seller_user_id);
+$query = $mysqli->prepare("SELECT  SUM(checkout_history.price) as revenue , products.seller_user_id  FROM checkout_history,products  WHERE (checkout_history.product_id= products.id and products.seller_user_id=? and checkout_history.checkout_date >= ?)");
+$query->bind_param("ss", $seller_user_id,$checkout_date);
 $query->execute();
 
 
