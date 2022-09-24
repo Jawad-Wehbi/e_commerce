ecaa2105d193c5014e45144e31b7460787312a96
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-25 01:35:25 +0300

    Updating API  to be able to receive axios data

--- view_favourite.php
+++ view_favourite.php
@@ -4,8 +4,11 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connections.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
-$client = $_POST["client_user_id"];
+
+$client = $data["client_user_id"];
 
 $query = $mysqli->prepare("SELECT name, image, price, nb_of_views, rating, category
  FROM products INNER JOIN client_favorite_products ON products.id = client_favorite_products.product_id , client_id = ?");
