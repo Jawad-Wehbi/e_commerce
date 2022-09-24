79fd2726229f87b3ad1db14473a9786ddf2fbd70
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-25 01:31:11 +0300

    Updating API  to be able to receive axios data

--- edit_profile.php
+++ edit_profile.php
@@ -4,10 +4,13 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connections.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
-$name = $_POST["client_name"];
-$email = $_POST["client_email"];
-$pass = hash("sha256" ,$_POST["client_name"]);
+
+$name = $data["client_name"];
+$email = $data["client_email"];
+$pass = hash("sha256" ,$data["client_name"]);
 
 $query = $mysqli->prepare("UPDATE clients SET client_name = ?, client_email = ?, client_password = ?, ");
 $query->bind_param("sss", $name, $email, $password);
