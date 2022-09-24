002e2c8cae106e1f14143c29cb2724b6e676d414
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-25 01:38:41 +0300

    Updating API  to be able to receive axios data

--- add_cart.php
+++ add_cart.php
@@ -4,6 +4,9 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 include("connections.php");
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
+
 
 
 $query = $mysqli->prepare("INSERT INTO `carts` (`id`) VALUES (NULL);");
