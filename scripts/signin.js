43dcde177579decae111e866afef4cf755233f9f
JaafarHawli <jaafarhawle@gmail.com>
2022-09-24 14:06:57 +0300

    added comments

--- scripts/signin.js
+++ scripts/signin.js
@@ -3,12 +3,13 @@ const signinBlock = document.querySelector('.signin-block');
 const signupBlock = document.querySelector('.signup-block');
 const signupBack = document.querySelector('.signup-back');
 
-// Sign up link inside Sign in page
+// Sign up link
 signupLink.addEventListener('click', () => {
 	signinBlock.classList.add('display');
 	signupBlock.classList.remove('display');
 });
 
+// Back arrow svg in sign up block
 signupBack.addEventListener('click', () => {
 	signinBlock.classList.remove('display');
 	signupBlock.classList.add('display');
