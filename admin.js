aba643886acd7396080a50a8ef108123edcd86a8
Jawad-Wehbi <112473373+Jawad-Wehbi@users.noreply.github.com>
2022-09-25 23:22:53 +0300

    Bug Fixing

--- admin.js
+++ admin.js
@@ -17,13 +17,57 @@ window.onclick = function(event) {
     }
 }
 
-const navTab=document.querySelectorall(".nav-tab");
-const sections=document.querySelectorall(".page");
-navTab.forEach((element) => {
+console.log('test');
+
+const navTab = document.querySelectorAll(".nav-tab");
+const sellersTable = document.getElementById('sellersTable');
+const client = document.querySelectorAll(".client");
+const statistics = document.getElementById("statistics");
+const clientTable = document.getElementById("clientsTable");
+const table = document.getElementById("table");
+/* navTab.forEach((element) => {
     element.addEventListener('click', () => {
     sections.forEach((element) => element.classList.add('display'));
     sections[0].classList.remove('display');
+ */
 
+document.onload = function () {
+    sellersTable.classList.remove('display');
+    clientTable.classList.add('display');
+    table.classList.add('display');
+    statistics.classList.add('display');
+}
+
+// $(window).load(function() {
+//     sellersTable.classList.remove('display');
+//     clientTable.classList.add('display');
+//     table.classList.add('display');
+//     statistics.classList.add('display');
+// });
+
+navTab[0].addEventListener('click', () => {
+    sellersTable.classList.remove('display');
+    clientTable.classList.add('display');
+    table.classList.add('display');
+    statistics.classList.add('display');
 });
 
+navTab[1].addEventListener('click', () => {
+    sellersTable.classList.add('display');
+    clientTable.classList.remove('display');
+    table.classList.remove('display');
+    statistics.classList.add('display');
 });
+
+navTab[2].addEventListener('click', () => {
+    sellersTable.classList.add('display');
+    clientTable.classList.add('display');
+    table.classList.add('display');
+    statistics.classList.remove('display');
+});
+
+// });
+/* const navTab=document.querySelectorall(".nav-tab");
+const sections=document.querySelectorall(".page");
+navTab.forEach((element) => {
+    element.addEventListener('click', () => { */
