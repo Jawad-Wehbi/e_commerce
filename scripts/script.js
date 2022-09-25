851a5c40e1b6dc3d73dfee28c13233f67b2cbd3e
JaafarHawli <jaafarhawle@gmail.com>
2022-09-25 18:32:59 +0300

    ECW-23 added functionality to add to favourites button

--- scripts/script.js
+++ scripts/script.js
@@ -498,3 +498,58 @@ axios.post('http://localhost/client-backend/view-wishlist.php', { client_user_id
 		});
 	});
 });
+
+// View Favourites
+axios.post('http://localhost/client-backend/view-wishlist.php', { client_user_id: localStorage.userId }).then((res) => {
+	console.log(res.data);
+	res.data.forEach((product) => {
+		let id = { product_id: product.products_id };
+		axios.post('http://localhost/client-backend/view_product.php', id).then((res2) => {
+			wishlistItems.innerHTML += `
+		<!-- Product -->
+		<div class="product-container" >
+			<div class="product light-navy pointer">
+				<div  onclick="showProduct(${res2.data[0].id})">
+					<!-- Product image -->
+					<div class="product-image-container flex">
+						<img src="${res2.data[0].image}" alt="product" class="product-image">
+					</div>
+					<!-- Product category -->
+					<h4 class="product-category">${res2.data[0].category_name}</h4>
+					<!-- Product name -->
+					<h3 class="product-name">${res2.data[0].product_name}</h3>
+					<!-- Product price -->
+					<h3 class="price white-font">${res2.data[0].price}</h3>
+				</div>
+				
+				<!-- Product favourites / wishlist icons -->
+				<div class="product-icons flex">
+					<!-- Favourites -->
+					<div onclick="addFavourite(${res2.data[0].id})">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="favourites pointer"><path fill="#001743" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+					</div>
+					
+					<!-- Wishlist -->
+					<div onclick="addWishlist(${res2.data[0].id})">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="wishlist-product" ><path fill="#001743" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
+					</div>
+				</div>
+				<!-- Product add to cart action -->
+				<div class="product-cart navy">
+					<button type="button" class="button add-to-cart">Add To Cart</button> 
+				</div>
+			</div>
+		</div>`;
+		});
+	});
+});
+
+// Add favourite
+function addFavourite(id) {
+	console.log(localStorage.userId);
+	let user = localStorage.userId;
+	let productId = { products_id: id, client_user_id: user };
+	axios.post('http://localhost/client-backend/add_favourite.php', productId).then((res) => {
+		console.log(res.data);
+	});
+}
