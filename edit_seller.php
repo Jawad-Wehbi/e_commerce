9b77eab1d298ef828dd47886e4dca018bf977374
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-25 14:42:03 +0300

    fixed naming issue

--- edit_seller.php
+++ edit_seller.php
@@ -12,15 +12,11 @@ $data = json_decode($request_body, true);
 
 //prepare inputs from the user
 $user_id = $data["user_id"];
-
+$seller_name = $data["seller_name"];
 
 //Update the sellers name in the database
-$query = $mysqli->prepare("DELETE  FROM sellers WHERE user_id=?");
-$query->bind_param("ss",$user_id );
-$query->execute();
-
-$query = $mysqli->prepare("DELETE  FROM users WHERE id=?");
-$query->bind_param("s",$user_id );
+$query = $mysqli->prepare("UPDATE  sellers SET seller_name =? WHERE user_id=?");
+$query->bind_param("ss",$user_id,$seller_name );
 $query->execute();
 
 
