462332bce536728f24cabdfdbbe2654b8662ea9b
JaafarHawli <jaafarhawle@gmail.com>
2022-09-26 14:16:16 +0300

    removed add to cart box from products

--- client-frontend/scripts/script.js
+++ client-frontend/scripts/script.js
@@ -307,10 +307,6 @@ axios.get('http://localhost/client-backend/new_products.php').then((res) => {
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="wishlist-product" ><path fill="#001743" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
 						</div>
 					</div>
-					<!-- Product add to cart action -->
-					<div class="product-cart navy">
-						<button type="button" class="button add-to-cart" onclick="addToCart(${product.id})">Add To Cart</button> 
-					</div>
 				</div>
 			</div>`;
 	});
@@ -375,10 +371,6 @@ axios.get('http://localhost/client-backend/top-selling.php').then((res) => {
 					<div onclick="addWishlist(${product.id})">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="wishlist-product" ><path fill="#001743" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
 					</div>
-				</div>
-				<!-- Product add to cart action -->
-				<div class="product-cart navy">
-					<button type="button" class="button add-to-cart" onclick="addToCart(${product.id})">Add To Cart</button> 
 				</div>
 			</div>
 		</div>`;
@@ -482,10 +474,6 @@ axios.post('http://localhost/client-backend/view_wishlist.php', { client_user_id
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="wishlist-product" ><path fill="#001743" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
 					</div>
 				</div>
-				<!-- Product add to cart action -->
-				<div class="product-cart navy">
-					<button type="button" class="button add-to-cart" onclick="addToCart(${product.id})">Add To Cart</button> 
-				</div>
 			</div>
 		</div>`;
 		});
@@ -529,10 +517,6 @@ axios
 							<div onclick="addWishlist(${id2})">
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="wishlist-product" ><path fill="#001743" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
 							</div>
-						</div>
-						<!-- Product add to cart action -->
-						<div class="product-cart navy">
-							<button type="button" class="button add-to-cart" onclick="addToCart(${product.id})"> Add To Cart</button> 
 						</div>
 					</div>
 				</div>`;
@@ -582,10 +566,6 @@ function viewCategory(id) {
 					<div onclick="addWishlist(${product.id})">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="wishlist-product" ><path fill="#001743" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
 					</div>
-				</div>
-				<!-- Product add to cart action -->
-				<div class="product-cart navy">
-					<button type="button" class="button" add-to-cart onclick="addToCart(${product.id})">Add To Cart</button> 
 				</div>
 			</div>
 		</div>`;
